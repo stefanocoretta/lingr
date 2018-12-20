c453942b6039a9944898ace174536c43f5c9add8
Stefano Coretta <stefanoc.social@me.com>
2018-12-20 08:29:30 +0100

    document package

---
+++ R/lingr.R
@@ -0,0 +1,8 @@
+#' lingr: Template For Linguistics Articles.
+#'
+#' This package provides a basic RMarkdown template for linguisics articles
+#' (internal use).
+#'
+#' @docType package
+#' @name lingr
+NULL
