079df31c7243d2a2918122f29d135720c7f2aacf
Stefano Coretta <stefanoc.social@me.com>
2019-05-11 14:04:56 +0200

    :sparkles: use params, print version and date, extend doc

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,10 @@
 #' Lingr custom format.
 #'
-#' A custom format for linguistics articles.
+#' A custom format for linguistics articles. It provides the standard pandoc
+#' YAML parameter, plus three new parameters: \code{version} for the document's
+#' version, \code{version-date} for the date of the document's version (these parameters must be included as subkeys in \code{params}), and
+#' \code{hide-version} which does not print the version on the document if set
+#' to \code{true}.
 #'
 #' @param ... Arguments passed to \link[rmarkdown]{pdf_document}.
 #'
