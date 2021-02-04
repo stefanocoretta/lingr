444db1128efd899b46eb640554d9224b27d085e4
stefanocoretta <stefano.coretta@gmail.com>
2021-02-04 22:00:04 +0100

    :tada: - clean up

--- R/lingr.R
+++
@@ -1,8 +0,0 @@
-#' lingr: Template For Linguistics Articles.
-#'
-#' This package provides a basic RMarkdown template for linguisics articles
-#' (internal use).
-#'
-#' @docType package
-#' @name lingr
-NULL
